f214c72ae56e061e3127b198990b124e3b963f68
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-05 15:32:30 +0900

    Title: 거울에 레이저 쏘기 2, Time: 74ms, Memory: 28MB, Status: Wrong Answer - Codetree

--- 250705/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 250705/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -5,29 +5,28 @@ for _ in range(map_size):
     total_map.append(cur_row)
 start_num = int(input())
 
-first_dx = [-1, 1, 0, 0]    # /
+first_dx = [-1, 1, 0, 0]    # /, 북 남 서 동
 first_dy = [0, 0, -1, 1]    # /
 
-second_dx = [-1, 1, 0, 0]   # \
+second_dx = [-1, 1, 0, 0]   # \, 북 남 동 서
 second_dy = [0, 0, 1, -1]   # \
 
 first_to_second = {0: 0, 1: 1, 2: 3, 3: 2}
 second_to_first = {0: 0, 1: 1, 2: 3, 3: 2}
 
-which_side = (start_num-1) // map_size
-if which_side == 0:
+if 1 <= start_num <= map_size:
     cur_row = 0
     cur_col = start_num - 1
     cur_dir = 1
-elif which_side == 1:
+elif (map_size + 1) <= start_num <= (2 * map_size):
     cur_col = map_size - 1
     cur_row = start_num - map_size
     cur_dir = 2
-elif which_side == 2:
+elif (2 * map_size + 1) <= start_num <= (3 * map_size):
     cur_row = start_num - 1
     cur_col = map_size * 3 - start_num
     cur_dir = 0
-elif which_side == 3:
+elif (3 * map_size + 1) <= start_num <= (4 * map_size):
     cur_col = 0
     cur_row = map_size * 4 - start_num
     cur_dir = 3
