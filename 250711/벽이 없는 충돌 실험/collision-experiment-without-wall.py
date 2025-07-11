8a13c0887758b05aa1f446563c913aaed080f97b
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-11 13:59:43 +0900

    Title: 벽이 없는 충돌 실험, Time: 57ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250711/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 250711/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,3 +1,5 @@
+import time
+
 total_testcase = int(input())
 
 dir_dict = {"U": 0, "D": 1, "R": 2, "L": 3}
@@ -7,10 +9,11 @@ dy = [1, -1, 0, 0]
 
 INF = int(1e9)
 
+
 def visualize(ball_list, min_x, max_x, min_y, max_y):
     print(f"<x: {min_x}~{max_x} / y: {min_y}~{max_y}>")
-    total_map_ball_idx = [["x"] * (max_x-min_x+1) for _ in range(max_y-min_y+1)]
-    total_map_ball_dir = [["x"] * (max_x-min_x+1) for _ in range(max_y-min_y+1)]
+    total_map_ball_idx = [["x"] * (max_x - min_x + 1) for _ in range(max_y - min_y + 1)]
+    total_map_ball_dir = [["x"] * (max_x - min_x + 1) for _ in range(max_y - min_y + 1)]
 
     for ball_idx, cur_ball_list in enumerate(ball_list):
         cur_y = cur_ball_list[1]
@@ -18,9 +21,9 @@ def visualize(ball_list, min_x, max_x, min_y, max_y):
         cur_weight = cur_ball_list[2]
         cur_idx = cur_ball_list[-1]
         cur_dir = dir_str_dict[cur_ball_list[-2]]
-        total_map_ball_idx[max_y-cur_y][cur_x-min_x] = cur_idx
-        total_map_ball_dir[max_y-cur_y][cur_x-min_x] = cur_dir
-    
+        total_map_ball_idx[max_y - cur_y][cur_x - min_x] = cur_idx
+        total_map_ball_dir[max_y - cur_y][cur_x - min_x] = cur_dir
+
     for row in total_map_ball_idx:
         print('\t'.join(map(str, row)))
     print()
@@ -29,35 +32,49 @@ def visualize(ball_list, min_x, max_x, min_y, max_y):
     print()
     print()
 
+
 def move(ball_list, min_x, max_x, min_y, max_y):
     hit = False
     next_ball_list = []
-    tmp_map = [["x"] * (max_x-min_x+1) for _ in range(max_y-min_y+1)]
+    # now = time.time()
+    # tmp_map = [["x"] * (max_x - min_x + 1) for _ in range(max_y - min_y + 1)]
     for cur_ball_list in ball_list:
         # x, y, weight, direction, ball_idx
         cur_x, cur_y, cur_weight, cur_dir, cur_idx = cur_ball_list[0], cur_ball_list[1], cur_ball_list[2], cur_ball_list[3], cur_ball_list[4]
         next_x = cur_x + dx[cur_dir]
         next_y = cur_y + dy[cur_dir]
+
         if min_x <= next_x <= max_x and min_y <= next_y <= max_y:
-            if tmp_map[max_y-next_y][next_x-min_x] == "x":
-                tmp_map[max_y-next_y][next_x-min_x] = [next_x, next_y, cur_weight, cur_dir, cur_idx]
+            if tmp_map[max_y - next_y][next_x - min_x] == "x":
+                tmp_map[max_y - next_y][next_x - min_x] = [next_x, next_y, cur_weight, cur_dir, cur_idx]
+                next_ball_list.append([next_x, next_y, cur_weight, cur_dir, cur_idx])
             else:
                 hit = True
-                other_ball_list = tmp_map[max_y-next_y][next_x-min_x]
+                other_ball_list = tmp_map[max_y - next_y][next_x - min_x]
                 other_ball_weight, other_ball_idx = other_ball_list[2], other_ball_list[-1]
                 if other_ball_weight < cur_weight:
-                    tmp_map[max_y-next_y][next_x-min_x] = [next_x, next_y, cur_weight, cur_dir, cur_idx]
+                    tmp_map[max_y - next_y][next_x - min_x] = [next_x, next_y, cur_weight, cur_dir, cur_idx]
+                    for idx, _ball_list in enumerate(next_ball_list):
+                        if _ball_list[-1] == other_ball_idx:
+                            next_ball_list[idx] = [next_x, next_y, cur_weight, cur_dir, cur_idx]
                 elif other_ball_weight == cur_weight:
                     if cur_idx > other_ball_idx:
-                        tmp_map[max_y-next_y][next_x-min_x] = [next_x, next_y, cur_weight, cur_dir, cur_idx]
-    for y in range(len(tmp_map)):
-        for x in range(len(tmp_map[0])):
-            if tmp_map[y][x] != "x":
-                next_ball_list.append(tmp_map[y][x])
+                        tmp_map[max_y - next_y][next_x - min_x] = [next_x, next_y, cur_weight, cur_dir, cur_idx]
+                        for idx, _ball_list in enumerate(next_ball_list):
+                            if _ball_list[-1] == other_ball_idx:
+                                next_ball_list[idx] = [next_x, next_y, cur_weight, cur_dir, cur_idx]
+    # print(time.time()-now)
+
+    # for y in range(max_y - min_y + 1):
+    #     for x in range(max_x - min_x + 1):
+    #         if tmp_map[y][x] != "x":
+    #             next_ball_list.append(tmp_map[y][x])
+    #             tmp_map[y][x] = "x"
+
     return hit, next_ball_list
 
+
 def check(ball_list, min_x, max_x, min_y, max_y):
-    
     return (len(ball_list) == 0)
 
 
@@ -72,7 +89,8 @@ for testcase_idx in range(total_testcase):
         max_x = max(max_x, int(cur_ball_info[0]))
         min_y = min(min_y, int(cur_ball_info[1]))
         max_y = max(max_y, int(cur_ball_info[1]))
-        cur_ball_info_list = [int(cur_ball_info[0]), int(cur_ball_info[1]), int(cur_ball_info[2]), cur_ball_dir, ball_idx]  # x, y, weight, direction, ball_idx
+        cur_ball_info_list = [int(cur_ball_info[0]), int(cur_ball_info[1]), int(cur_ball_info[2]), cur_ball_dir,
+                              ball_idx]  # x, y, weight, direction, ball_idx
         ball_list.append(cur_ball_info_list)
     for ball_idx, cur_ball_list in enumerate(ball_list):
         cur_ball_list[0] = min_x + (cur_ball_list[0] - min_x) * 2
@@ -83,15 +101,17 @@ for testcase_idx in range(total_testcase):
 
     result = -1
     cur_time = 0
+    # visualize(ball_list, min_x, max_x, min_y, max_y)
+    tmp_map = [["x"] * (max_x - min_x + 1) for _ in range(max_y - min_y + 1)]
     while True:
-        
-        for move_idx in range(2):
-            cur_time += 1
-            hit, ball_list = move(ball_list, min_x, max_x, min_y, max_y)
-            # visualize(ball_list, min_x, max_x, min_y, max_y)
-            if hit:
-                result = cur_time
+        # now = time.time()
+        cur_time += 1
+
+        hit, ball_list = move(ball_list, min_x, max_x, min_y, max_y)
+        if hit:
+            result = cur_time
 
         if check(ball_list, min_x, max_x, min_y, max_y):
             break
+        # print(time.time()-now)
     print(result)
