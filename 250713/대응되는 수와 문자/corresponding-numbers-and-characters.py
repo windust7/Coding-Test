bebc5bca8aab82efc5ecb1fb248618b84f5cac0b
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-13 14:53:21 +0900

    Title: 대응되는 수와 문자, Time: 245ms, Memory: 37MB, Status: Passed - Codetree

--- 250713/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 250713/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -5,8 +5,13 @@ words = [""] + [input() for _ in range(n)]
 queries = [input() for _ in range(m)]
 
 # Please write your code here.
+_dict = {}
+
+for idx, word in enumerate(words[1:]):
+    _dict[word] = idx+1
+
 for query in queries:
     if query.isalpha():
-        print(words.index(query))
+        print(_dict[query])
     else:
         print(words[int(query)])
