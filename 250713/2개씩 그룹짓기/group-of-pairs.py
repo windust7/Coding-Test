cccb83af78b1c98aa35a179aade2cb82425076e5
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-13 17:34:41 +0900

    Title: 2개씩 그룹짓기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250713/2개씩 그룹짓기/group-of-pairs.py
@@ -0,0 +1,15 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+# Please write your code here.
+nums.sort()
+original = nums[:]
+nums.sort(reverse=True)
+# print(nums, original)
+
+_max = -int(1e9)
+
+for idx in range(n//2+1):
+    _max = max(_max, original[idx]+nums[idx])
+
+print(_max)
