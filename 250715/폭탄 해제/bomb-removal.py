b5f1e6da8b4c29e78eea929fe3a8be965136228c
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-15 10:10:18 +0900

    Title: 폭탄 해제, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250715/폭탄 해제/bomb-removal.py
@@ -0,0 +1,5 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+# Please write your code here.
+print(f"code : {unlock_code}\ncolor : {wire_color}\nsecond : {seconds}")
