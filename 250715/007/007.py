2310ff993193175451abd9e3382d5443d87217cc
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-15 10:04:12 +0900

    Title: 007, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250715/007/007.py
@@ -0,0 +1,5 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Please write your code here.
+print(f"secret code : {secret_code}\nmeeting point : {meeting_point}\ntime : {time}")
