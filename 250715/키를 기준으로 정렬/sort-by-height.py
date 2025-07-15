950d98056d3dabdde7e09d9d6f89430abff20ab0
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-15 14:53:01 +0900

    Title: 키를 기준으로 정렬, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250715/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,24 @@
+n = int(input())
+name = []
+height = []
+weight = []
+
+for _ in range(n):
+    n_i, h_i, w_i = input().split()
+    name.append(n_i)
+    height.append(int(h_i))
+    weight.append(int(w_i))
+
+# Please write your code here.
+class Person():
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+_list = [Person(name[idx], height[idx], weight[idx]) for idx in range(n)]
+
+_list.sort(key=lambda x: x.height)
+
+for item in _list:
+    print(f"{item.name} {item.height} {item.weight}")
