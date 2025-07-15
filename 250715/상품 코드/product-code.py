197434b457e368947a413da9deb61be6e309f0df
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-15 10:12:07 +0900

    Title: 상품 코드, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250715/상품 코드/product-code.py
@@ -0,0 +1,6 @@
+product_name, product_code = input().split()
+product_code = int(product_code)
+
+# Please write your code here.
+print("product 50 is codetree")
+print(f"product {product_code} is {product_name}")
