9e4757322c00139e0dba8eb9b8d5a203ed2574bf
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-15 10:09:16 +0900

    Title: 코드네임, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250715/코드네임/code-name.py
@@ -0,0 +1,12 @@
+MAX_N = 5
+
+codenames = []
+scores = []
+for _ in range(MAX_N):
+    codename, score = input().split()
+    codenames.append(codename)
+    scores.append(int(score))
+
+# Please write your code here.
+idx = scores.index(min(scores))
+print(f"{codenames[idx]} {scores[idx]}")
