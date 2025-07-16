62f1ce53dbb4a73ca211a13230cf2bf3541a8a7f
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-16 12:59:13 +0900

    Title: 기수 정렬 구현, Time: 79ms, Memory: 20MB, Status: Wrong Answer - Codetree

---
+++ 250716/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+for cur_idx in range(6):
+    if cur_idx == 0:
+        cur_list = [[] for _ in range(10)]
+        for item in arr:
+            cur_list_idx = item % 10
+            cur_list[cur_list_idx].append(item)
+    else:
+        next_list = [[] for _ in range(10)]
+        for cur_list_idx in cur_list:
+            for item in cur_list_idx:
+                next_list_idx = int(str(item % (10 ** (cur_idx+1)))[0])
+                next_list[next_list_idx].append(item)
+        cur_list = next_list
+
+for cur_list_idx in cur_list:
+    for item in cur_list_idx:
+        print(item, end=" ")
