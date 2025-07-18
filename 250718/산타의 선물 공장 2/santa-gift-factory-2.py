219711237edff8c62a7ec32d850ba3d3279592af
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-18 19:23:57 +0900

    Title: 산타의 선물 공장 2, Time: 229ms, Memory: 30MB, Status: Runtime Error - Codetree

--- 250718/산타의 선물 공장 2/santa-gift-factory-2.py
+++ 250718/산타의 선물 공장 2/santa-gift-factory-2.py
@@ -21,6 +21,8 @@
     6-1. 물건이 없으면 -1로 대체
 """
 
+CHECK = False
+
 num_cmd = int(input())
 
 class Node:
@@ -48,7 +50,7 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
         self.num_node = 0
-    
+
     def push_front(self, new_node):
         if self.num_node == 0:
             self.head = new_node
@@ -60,7 +62,7 @@ class DoublyLinkedList:
             connect(new_node, self.head)
             self.head = new_node
         self.num_node += 1
-    
+
     def push_back(self, new_node):
         if self.num_node == 0:
             self.head = new_node
@@ -72,7 +74,7 @@ class DoublyLinkedList:
             connect(self.tail, new_node)
             self.tail = new_node
         self.num_node += 1
-    
+
     def pop_front(self):
         if self.num_node == 0:
             return None
@@ -85,12 +87,224 @@ class DoublyLinkedList:
         else:
             return_node = self.head
             self.head = return_node.next
+            disconnect(return_node, self.head)
             self.num_node -= 1
             return return_node
+
+    def __str__(self):
+        if self.num_node == 0:
+            return "EMPTY\n"
+        else:
+            return_str = f"total: {self.num_node}\n"
+            cur_node = self.head
+            while True:
+                return_str += f"{cur_node.data} "
+                if cur_node == self.tail:
+                    if cur_node.next is not None:
+                        print(f"****************************** {cur_node} -> {cur_node.next} ******************************")
+                    break
+                else:
+                    cur_node = cur_node.next
+            return_str += "\n"
+            return return_str
+
+def visualize():
+    for idx, belt in enumerate(belt_list[1:]):
+        print(f"belt {idx + 1}:")
+        print(belt)
 
 first_cmd = list(map(int, input().split()))
 assert first_cmd[0] == 100
 num_belt, num_gift = first_cmd[1], first_cmd[2]
+belt_list = [DoublyLinkedList() for _ in range(num_belt+1)]
+gift_dict = {}
+for item_idx, belt_idx in enumerate(first_cmd[3:]):
+    new_gift = Node(item_idx+1)
+    belt_list[belt_idx].push_back(new_gift)
+    gift_dict[item_idx+1] = new_gift
+
+if CHECK:
+    visualize()
+
+def action_2(cmd_list):
+    m_src = cmd_list[1]
+    m_dst = cmd_list[2]
+
+    src_belt = belt_list[m_src]
+    dst_belt = belt_list[m_dst]
+
+    if src_belt.num_node == 0:
+        pass
+    elif src_belt.num_node == 1:
+        move_gift = src_belt.head
+        src_belt.head = None
+        src_belt.tail = None
+        src_belt.num_node = 0
+        dst_belt.push_front(move_gift)
+    else:
+        move_start = src_belt.head
+        move_end = src_belt.tail
+        src_belt.head = None
+        src_belt.tail = None
+
+        connect(move_end, dst_belt.head)
+        dst_belt.head = move_start
+
+        dst_belt.num_node += src_belt.num_node
+        src_belt.num_node = 0
+    print(dst_belt.num_node)
+
+def action_3(cmd_list):
+    m_src = cmd_list[1]
+    m_dst = cmd_list[2]
+
+    src_belt = belt_list[m_src]
+    dst_belt = belt_list[m_dst]
+
+    if src_belt.num_node == 0:
+        if dst_belt.num_node == 0:
+            pass
+        elif dst_belt.num_node == 1:
+            from_dst = dst_belt.pop_front()
+            src_belt.push_front(from_dst)
+        else:
+            from_dst = dst_belt.pop_front()
+            src_belt.push_front(from_dst)
+    elif src_belt.num_node == 1:
+        if dst_belt.num_node == 0:
+            from_src = src_belt.pop_front()
+            dst_belt.push_front(from_src)
+        elif dst_belt.num_node == 1:
+            from_src = src_belt.pop_front()
+            from_dst = dst_belt.pop_front()
+            dst_belt.push_front(from_src)
+            src_belt.push_front(from_dst)
+        else:
+            from_src = src_belt.pop_front()
+            from_dst = dst_belt.pop_front()
+            dst_belt.push_front(from_src)
+            src_belt.push_front(from_dst)
+    else:
+        if dst_belt.num_node == 0:
+            from_src = src_belt.pop_front()
+            dst_belt.push_front(from_src)
+        elif dst_belt.num_node == 1:
+            from_src = src_belt.pop_front()
+            from_dst = dst_belt.pop_front()
+            dst_belt.push_front(from_src)
+            src_belt.push_front(from_dst)
+        else:
+            from_src = src_belt.pop_front()
+            from_dst = dst_belt.pop_front()
+            dst_belt.push_front(from_src)
+            src_belt.push_front(from_dst)
+    print(dst_belt.num_node)
+
+def action_4(cmd_list):
+    m_src = cmd_list[1]
+    m_dst = cmd_list[2]
+
+    src_belt = belt_list[m_src]
+    dst_belt = belt_list[m_dst]
+
+    if src_belt.num_node == 0 or src_belt.num_node == 1:
+        pass
+    else:
+        num_move = src_belt.num_node // 2
+        move_start_gift = src_belt.head
+        move_end_gift = src_belt.head
+        for _ in range(num_move-1):
+            move_end_gift = move_end_gift.next
+
+        if move_start_gift == move_end_gift:
+            src_belt.head = move_start_gift.next
+            disconnect(move_start_gift, src_belt.head)
+            if dst_belt.num_node == 0:
+                dst_belt.head = move_start_gift
+                dst_belt.tail = move_start_gift
+            elif dst_belt.num_node == 1:
+                connect(move_end_gift, dst_belt.head)
+                dst_belt.head = move_start_gift
+            else:
+                connect(move_end_gift, dst_belt.head)
+                dst_belt.head = move_start_gift
+        else:
+            src_belt.head = move_end_gift.next
+            disconnect(move_end_gift, src_belt.head)
+            if dst_belt.num_node == 0:
+                dst_belt.head = move_start_gift
+                dst_belt.tail = move_end_gift
+            elif dst_belt_num_node == 1:
+                conect(move_end_gift, dst_belt.head)
+                dst_belt.head = move_start_gift
+            else:
+                conect(move_end_gift, dst_belt.head)
+                dst_belt.head = move_start_gift
+
+        src_belt.num_node -= num_move
+        dst_belt.num_node += num_move
+
+    print(dst_belt.num_node)
+
+def action_5(cmd_list):
+    p_num = cmd_list[1]
+    target_gift = gift_dict[p_num]
+
+    if target_gift.prev is None:
+        a = -1
+    else:
+        a = target_gift.prev.data
+
+    if target_gift.next is None:
+        b = -1
+    else:
+        b = target_gift.next.data
+
+    print(a + 2 * b)
+
+def action_6(cmd_list):
+    b_num = cmd_list[1]
+    target_belt = belt_list[b_num]
+    if target_belt.num_node == 0:
+        a, b = -1, -1
+    else:
+        a = target_belt.head.data
+        b = target_belt.tail.data
+    c = target_belt.num_node
+    print(a + 2 * b + 3 * c)
 
 for cmd_idx in range(num_cmd-1):
-    pass
+    cur_cmd = list(map(int, input().split()))
+
+    if cur_cmd[0] == 200:
+        action_2(cur_cmd)
+        if CHECK:
+            print("==========")
+            print(f"belt {cur_cmd[1]} 전부 belt {cur_cmd[2]} 앞으로")
+            visualize()
+    elif cur_cmd[0] == 300:
+        action_3(cur_cmd)
+        if CHECK:
+            print("==========")
+            print(f"belt {cur_cmd[1]} 제일 앞이랑 belt {cur_cmd[2]} 제일 앞이랑 교환")
+            visualize()
+    elif cur_cmd[0] == 400:
+        action_4(cur_cmd)
+        if CHECK:
+            print("==========")
+            print(f"belt {cur_cmd[1]}의 앞에서 반개까지 belt {cur_cmd[2]} 제일 앞으로")
+            visualize()
+    elif cur_cmd[0] == 500:
+        action_5(cur_cmd)
+        if CHECK:
+            print("==========")
+            print(f"{cur_cmd[1]}의 선물의 앞은 a, 뒤는 b라고 할 때 a + 2 * b")
+            visualize()
+    elif cur_cmd[0] == 600:
+        action_6(cur_cmd)
+        if CHECK:
+            print("==========")
+            print(f"{cur_cmd[1]} 벨트의 제일 앞은 a, 뒤를 b, 선물의 개수를 c라고 할 때 a + 2 * b + 3 * c")
+            visualize()
+    else:
+        print(f"Wrong cmd: {cur_cmd}")
