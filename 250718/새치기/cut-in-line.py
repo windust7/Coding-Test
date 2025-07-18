0564fab49fc15ee5c1b3858861a322f14d6c3cf8
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-18 16:20:27 +0900

    Title: 새치기, Time: 109ms, Memory: 23MB, Status: Passed - Codetree

--- 250718/새치기/cut-in-line.py
+++ 250718/새치기/cut-in-line.py
@@ -14,10 +14,11 @@ CHECK = False
 num_people, num_queue, num_cmd = map(int, input().split())
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, is_separator=False):
         self.data = data
         self.prev = None
         self.next = None
+        self.is_separator = is_separator
 
     def __str__(self):
         return f"{self.data}"
@@ -112,24 +113,6 @@ class DoublyLinkedList:
                 connect(original_prev, node_b)
                 connect(node_c, node_a)
 
-    def pop(self, pop_node):
-        if pop_node == self.head:
-            if pop_node == self.tail:
-                self.head = None
-                self.tail = None
-            else:
-                self.head = pop_node.next
-                disconnect(pop_node, pop_node.next)
-        elif pop_node == self.tail:
-            self.tail = pop_node.prev
-            disconnect(pop_node.prev, pop_node)
-        else:
-            pop_node_prev = pop_node.prev
-            pop_node_next = pop_node.next
-            disconnect(pop_node_prev, pop_node)
-            disconnect(pop_node, pop_node_next)
-            connect(pop_node.prev, pop_node_next)
-
     def result_print(self):
         if self.head is None:
             print(-1)
@@ -144,18 +127,20 @@ class DoublyLinkedList:
                     cur_node = cur_node.next
             print(return_str)
 
-queue_list = [DoublyLinkedList() for _ in range(num_queue)]
+dll = DoublyLinkedList()
 node_dict = {}
-# node_queue_dict = {}
-node_queue_dict = [None for _ in range(num_people+1)]
+
+separator_node = Node(-1, is_separator=True)
+dll.push_back(separator_node)
 
 for queue_idx in range(num_queue):
     cur_queue = list(map(int, input().split()))
     for data in cur_queue[1:]:
         new_node = Node(data)
         node_dict[data] = new_node
-        node_queue_dict[data] = queue_idx
-        queue_list[queue_idx].push_back(new_node)
+        dll.push_back(new_node)
+    separator_node = Node(-1, is_separator=True)
+    dll.push_back(separator_node)
 
 for cmd_idx in range(num_cmd):
     cur_cmd = list(map(int, input().split()))
@@ -163,62 +148,58 @@ for cmd_idx in range(num_cmd):
         if CHECK:
             print(f"{cur_cmd[1]} -> {cur_cmd[2]}")
         node_a = node_dict[cur_cmd[1]]
-        node_a_queue = queue_list[node_queue_dict[cur_cmd[1]]]
-        # node_a_queue.pop(node_a)
-        if node_a == node_a_queue.head:
-            if node_a_queue.head == node_a_queue.tail:
-                node_a_queue.head = None
-                node_a_queue.tail = None
-            else:
-                node_a_queue.head = node_a.next
-        elif node_a == node_a_queue.tail:
-            node_a_queue.tail = node_a.prev
         node_b = node_dict[cur_cmd[2]]
-        node_b_queue = queue_list[node_queue_dict[cur_cmd[2]]]
-        node_b_queue.action_1(node_a, node_b)
-        node_queue_dict[cur_cmd[1]] = node_queue_dict[cur_cmd[2]]
+        dll.action_1(node_a, node_b)
     elif cur_cmd[0] == 2:
         if CHECK:
             print(f"{cur_cmd[1]} -> OUT")
         node_a = node_dict[cur_cmd[1]]
-        node_a_queue = queue_list[node_queue_dict[cur_cmd[1]]]
-        node_a_queue.action_2(node_a)
+        dll.action_2(node_a)
     elif cur_cmd[0] == 3:
         if CHECK:
             print(f"{cur_cmd[1]} ~ {cur_cmd[2]} -> {cur_cmd[3]}")
         node_a = node_dict[cur_cmd[1]]
         node_b = node_dict[cur_cmd[2]]
         node_c = node_dict[cur_cmd[3]]
-
-        node_a_queue = queue_list[node_queue_dict[cur_cmd[1]]]
-        node_c_queue = queue_list[node_queue_dict[cur_cmd[3]]]
-
-        if node_a == node_a_queue.head:
-            if node_a == node_a_queue.tail:
-                node_a_queue.head = None
-                node_a_queue.tail = None
-            else:
-                node_a_queue.head = node_b.next
-        if node_b == node_a_queue.tail:
-            node_a_queue.tail = node_a.prev
-
-        cur_node = node_a
-        change_queue_idx = node_queue_dict[cur_cmd[3]]
-        while True:
-            node_queue_dict[cur_node.data] = change_queue_idx
-            if cur_node == node_b:
-                break
-            else:
-                cur_node = cur_node.next
-        node_c_queue.action_3(node_a, node_b, node_c)
+        dll.action_3(node_a, node_b, node_c)
     else:
         print(f"Wrong cmd: {cur_cmd}")
 
     if CHECK:
         print()
-        for queue in queue_list:
-            queue.result_print()
+        cur_node = dll.head
+        while True:
+            if not cur_node.is_separator:
+                print(cur_node.data, end=" ")
+            else:
+                if cur_node == dll.tail:
+                    break
+                else:
+                    if cur_node.next.is_separator:
+                        print(-1)
+            if cur_node == dll.tail:
+                break
+            else:
+                cur_node = cur_node.next
         print()
 
-for queue in queue_list:
-    queue.result_print()
+cur_node = dll.head
+while True:
+    if not cur_node.is_separator:
+        print(cur_node.data, end=" ")
+    else:
+        if cur_node == dll.tail:
+            break
+        else:
+            if cur_node.next.is_separator:
+                print()
+                print(-1)
+                cur_node = cur_node.next
+                continue
+            else:
+                if cur_node != dll.head:
+                    print()
+    if cur_node == dll.tail:
+        break
+    else:
+        cur_node = cur_node.next
