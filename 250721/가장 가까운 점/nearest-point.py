881858d1b328a832cf241106295465f2ac79320a
ChangHyun, Choi <dkcharng@gmail.com>
2025-07-21 11:49:52 +0900

    Title: 가장 가까운 점, Time: 305ms, Memory: 26MB, Status: Wrong Answer - Codetree

---
+++ 250721/가장 가까운 점/nearest-point.py
@@ -0,0 +1,17 @@
+import heapq
+
+num_point, num_cmd = map(int, input().split())
+
+_list = []
+for _ in range(num_point):
+    cur_point = list(map(lambda x: int(x), input().split()))
+    input_tuple = tuple([cur_point[0] ** 2 + cur_point[1] ** 2] + cur_point)
+    heapq.heappush(_list, input_tuple)
+
+for cmd_idx in range(num_cmd):
+    cur_point = heapq.heappop(_list)
+    new_x, new_y = cur_point[1] + 2, cur_point[2] + 2
+    heapq.heappush(_list, (new_x ** 2 + new_y ** 2, new_x, new_y))
+
+result = heapq.heappop(_list)
+print(f"{result[1]} {result[2]}")
